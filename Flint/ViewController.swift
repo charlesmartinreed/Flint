d373f17a13bcf6005c0d6eea6b348379087efadf
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-25 22:52:55 -0600

    Remove Firebase pods. Implement Parse pods.

--- Flint/ViewController.swift
+++ Flint/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
+    
+    //MARK:- Properties
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
 
 }
 
